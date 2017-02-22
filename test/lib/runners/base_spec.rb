170a8bb8179a88c245c07c6068226cb68dfe2a3a
Imobach González Sosa <igonzalezsosa@suse.com>
2017-02-22 12:47:10 +0000

    Reduce duplication in Runners::Base tests

--- test/lib/runners/base_spec.rb
+++ test/lib/runners/base_spec.rb
@@ -5,36 +5,25 @@ require "cm/runners/base"
 
 describe Yast::CM::Runners::Base do
   subject(:runner) { Yast::CM::Runners::Base.new(config) }
-  let(:mode) { :masterless }
 
   let(:config) do
     { auth_attempts: 3, auth_time_out: 10, master: "some-server.suse.com", mode: mode }
   end
 
   describe "#run" do
-    context "when running in masterless mode" do
-      let(:mode) { :masterless }
+    let(:mode) { :masterless }
 
-      it "runs run_masterless_mode if defined" do
-        expect(runner).to receive(:run_masterless_mode).and_return(true)
-        runner.run
-      end
-
-      it "raises NotImplementedError" do
+    context "when a known mode is specified" do
+      it "raises a NotImplementedError error" do
         expect { runner.run }.to raise_error(NotImplementedError)
       end
     end
 
-    context "when running in client mode" do
-      let(:mode) { :client }
+    context "when a unknown mode is specified" do
+      let(:mode) { :unknown }
 
-      it "runs run_client_mode if defined" do
-        expect(runner).to receive(:run_client_mode).and_return(true)
-        runner.run
-      end
-
-      it "raises NotImplementedError" do
-        expect { runner.run }.to raise_error(NotImplementedError)
+      it "raises a" do
+        expect { runner.run }.to raise_error(NoMethodError)
       end
     end
   end
