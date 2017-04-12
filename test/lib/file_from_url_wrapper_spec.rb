6a0c2da796236b2d05b56be7d0a809c8d2880c20
Stefan Schubert <schubi@suse.de>
2017-04-12 17:24:48 +0200

    adapted testcases

--- test/lib/file_from_url_wrapper_spec.rb
+++ test/lib/file_from_url_wrapper_spec.rb
@@ -10,16 +10,36 @@ describe Yast::ConfigurationManagement::FileFromUrlWrapper do
     described_class { Yast::ConfigurationManagement::FileFromUrlWrapper }
     subject(:wrapper) { described_class }
 
+    let(:uri_port) { URI("http://yast.example.net:8888/some-file.txt") }
     let(:uri) { URI("http://yast.example.net/some-file.txt") }
+    let(:usb) { URI("usb:///some-file.txt") }
     let(:target) { Pathname("/tmp/local-file.txt") }
 
-    it "decompose URI and calls original get_file_from_url" do
+    it "decompose URI (with port) and calls original get_file_from_url" do
       expect(wrapper).to receive(:get_file_from_url).with(
-        scheme: "http", host: "yast.example.net",
+        scheme: "http", host: "yast.example.net:8888",
+        urlpath: "/some-file.txt", urltok: {}, destdir: "/",
+        localfile: "/tmp/local-file.txt"
+      )
+      wrapper.get_file(uri_port, target)
+    end
+
+    it "decompose URI (without port) and calls original get_file_from_url" do
+      expect(wrapper).to receive(:get_file_from_url).with(
+        scheme: "http", host: "yast.example.net:80",
         urlpath: "/some-file.txt", urltok: {}, destdir: "/",
         localfile: "/tmp/local-file.txt"
       )
       wrapper.get_file(uri, target)
+    end
+
+    it "decompose USB URI and calls original get_file_from_url" do
+      expect(wrapper).to receive(:get_file_from_url).with(
+        scheme: "usb", host: "",
+        urlpath: "/some-file.txt", urltok: {}, destdir: "/",
+        localfile: "/tmp/local-file.txt"
+      )
+      wrapper.get_file(usb, target)
     end
 
     it "returns value from get_file_from_url" do
