251628bf43c7188576411d552347cfeb578f75db
Imobach González Sosa <igonzalezsosa@suse.com>
2018-12-03 15:40:46 +0000

    Add a widget to group all form widgets

---
+++ src/lib/y2configuration_management/widgets/form.rb
@@ -0,0 +1,80 @@
+# encoding: utf-8
+
+# Copyright (c) [2018] SUSE LLC
+#
+# All Rights Reserved.
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of version 2 of the GNU General Public License as published
+# by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+# more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, contact SUSE LLC.
+#
+# To contact SUSE LLC about this file by physical or electronic mail, you may
+# find current contact information at www.suse.com.
+
+require "cwm"
+
+module Y2ConfigurationManagement
+  module Widgets
+    # Widget which represents a Salt Formula Form
+    #
+    # This acts as a container for those all the widgets that are related to a formula
+    # and takes care of:
+    #
+    # * Initializing the included widgets (see #value)...
+    # * and storing the final result (see #store and #result).
+    # It is able
+    class Form < ::CWM::CustomWidget
+      # @return [Array<CWM::AbstractWidget>] Widgets included in the form
+      attr_reader :children
+      # @return [Hash] Form values from included widgets when this one is removed from the UI
+      attr_reader :result
+
+      # Constructor
+      #
+      # @param children [Array<CWM::AbstractWidget>] Widgets included in the form
+      def initialize(children)
+        @children = children
+      end
+
+      # Sets the value for the form
+      #
+      # This method propagates the values to the underlying widgets.
+      #
+      # @example Setting values for included widgets
+      #   form.values = { "name" => "John", "surname" => "Doe" }
+      # @example Setting values for nested widgets
+      #   form.values = { "ranges" => [ { "start" => "10.0.0.10", "end" => "10.0.0.20" } ] }
+      #
+      # @return value [Hash] New value
+      def value=(value)
+        children.each do |widget|
+          widget.value = value[widget.id]
+        end
+      end
+
+      # Widget's content
+      #
+      # @see CWM::AbstractWidget
+      def contents
+        VBox(*children)
+      end
+
+      # Stores the widget's content
+      #
+      # The stored value can be obtained using the #result method
+      #
+      # @see CWM::AbstractWidget
+      def store
+        @result = children.reduce({}) { |h, w| h.merge(w.id => w.value) }
+      end
+    end
+  end
+end
