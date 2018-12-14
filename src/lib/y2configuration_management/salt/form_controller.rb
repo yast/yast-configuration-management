82491b1ba34f57da3444f8c9e5b29f7d3c38138d
Martin Vidner <mvidner@suse.cz>
2018-12-14 10:33:27 +0100

    Fixed editing collections of scalars AND hashes
    really the path handling should be abstracted away

--- src/lib/y2configuration_management/salt/form_controller.rb
+++ src/lib/y2configuration_management/salt/form_controller.rb
@@ -149,7 +149,8 @@ module Y2ConfigurationManagement
       def edit_item(path, item)
         element = form.find_element_by(path: path)
         widget_form = form_builder.build(element.prototype)
-        widget_form.value = item
+        wid = path[1..-1].split(".").last
+        widget_form.value = { wid => item }
         show_popup(element.name, widget_form)
       end
 
